125682fd20f260e35f8b5b3b560e00a97d84fa74
Manuel Duarte  <carlosd99m@gmail.com>
2023-09-10 00:13:13 -0600

    fix: using implements

--- lib/view/widgets/custom_appbar.dart
+++ lib/view/widgets/custom_appbar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String? title;
   final tap;
 
